b11a27d6af385bb77460cac80175edb9c3ab4c0a
truecallerabreham <truecallerabreham@gmail.com>
2025-07-11 15:58:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if query:
     results = collection.query(query_embeddings=[query_embedding.tolist()], n_results=2)
 
     st.subheader(" Top Matching Documents:")
-print(result["metadatas"])
+print(results["metadatas"])
 
 
 
